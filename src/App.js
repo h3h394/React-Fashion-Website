6e5dde9c42211b5d92c49fa96ea43df7ddcddc24
Milos <jasammimi1994@gmail.com>
2020-05-21 20:03:30 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SideText from "./components/GlobalComponents/SideText";
+import AboutModal from "./components/About/AboutModal";
+import Hero from "./components/Hero/Hero";
+import Trending from "./components/Trending/Trending";
+import About from "./components/About/About";
+import Products from "./components/Products/Products";
+import SummerSale from "./components/SummerSale/SummerSale";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Blog from "./components/Blog/Blog";
+import Stats from "./components/Stats/Stats";
+import Service from "./components/Service/Service";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AboutModal showModal={showModal} setShowModal={setShowModal} />
+      <SideText />
+      <Hero />
+      <Trending />
+      <About showModal={showModal} setShowModal={setShowModal} />
+      <Products />
+      <SummerSale />
+      <Testimonials />
+      <Blog />
+      <Stats />
+      <Service />
+      <Subscribe />
+      <Footer />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
